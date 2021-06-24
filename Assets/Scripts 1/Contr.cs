27e31babe2a2334b64c735c5580a4d526aa4fb95
H-A-K-E-R <drivito23@mail.ru>
2021-06-24 15:16:30 +0500

    1

---
+++ Assets/Scripts 1/Contr.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Contr : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    RaycastHit _hit;
+    private string _isGrounded = "Ground";
+    private bool _right;
+    private Rigidbody _rigidbody;
+ 
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        moveControl();
+        jumpControl();
+    }
+
+
+    private void moveControl()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        transform.Translate(movement * _speed * Time.fixedDeltaTime);
+    }
+
+    private void jumpControl()
+    {
+        Ray raycast1 = new Ray(transform.position, Vector3.down.normalized);
+        Physics.Raycast(raycast1, out _hit, 0.5f);
+        if (_isGrounded == _hit.collider.tag)
+        {
+            if (_hit.distance < 0.5f)
+            {
+                _right = true;
+                if (_right == true)
+                {
+                    if (Input.GetKey("space"))
+                    {
+                        _rigidbody.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+                    }
+
+                }
+            }
+        }
+        if (_right == true)
+        {
+            if (Input.GetKey("space"))
+            {
+                _rigidbody.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+            }
+        
+        }
+    }
+}   
---
+++ Assets/Scripts 1/Grenade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Grenade : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _grenade;
+    private Transform _grenadeSpawner;
+    [SerializeField] private float _force;
+    private Rigidbody _rigidbody;
+ 
+
+
+    private void Start()
+    {
+        this._rigidbody = GetComponent<Rigidbody>();
+        _grenadeSpawner = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        grenadeThrow();
+    }
+    private void grenadeThrow()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _rigidbody = ((GameObject)Instantiate(_grenade, _grenadeSpawner.position, _grenadeSpawner.rotation)).GetComponent<Rigidbody>();
+            _rigidbody.AddForce(transform.forward * _force, ForceMode.Impulse);
+        }
+    }
+}
